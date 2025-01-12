251feff6f9c56465b34d9189a801a2000cf080df
Ru2Lu <jiutianlian0@gmail.com>
2025-01-12 13:54:29 +0900

    Add navigation to DateInputScreen on long press of battery indicator

--- lib/screens/lifespan_progress_screen.dart
+++ lib/screens/lifespan_progress_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:life_battery/screens/date_input_screen.dart';
 import 'package:life_battery/widgets/battery_indicator.dart';
 
 /// Screen for showing the remaining of lifespan
@@ -10,8 +11,18 @@ class LifespanProgressScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: const Center(
-        child: BatteryIndicator(value: 100),
+      body: Center(
+        child: GestureDetector(
+          onLongPress: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute<void>(
+                builder: (context) => const DateInputScreen(),
+              ),
+            );
+          },
+          child: const BatteryIndicator(value: 100),
+        ),
       ),
     );
   }
