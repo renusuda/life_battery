b1dc7d6aee89dd59692d34f4773729c21c9d8e87
Ru2Lu <jiutianlian0@gmail.com>
2025-03-15 15:03:00 +0900

    Hide debug banner

--- lib/widgets/common_material_app.dart
+++ lib/widgets/common_material_app.dart
@@ -21,6 +21,7 @@ class CommonMaterialApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Life Battery',
       theme: lightModeTheme,
       darkTheme: darkModeTheme,
