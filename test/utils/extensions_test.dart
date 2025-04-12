2aa1bcf3570e1badfa1100043cdc3b1448152a9b
Ru2Lu <jiutianlian0@gmail.com>
2025-04-12 09:36:24 +0900

    Use flutter_test package

--- test/utils/extensions_test.dart
+++ test/utils/extensions_test.dart
@@ -1,5 +1,5 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:life_battery/utils/extensions.dart';
-import 'package:test/test.dart';
 
 void main() {
   group('IntExtensions', () {
