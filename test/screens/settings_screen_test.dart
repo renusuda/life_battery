ab9cf2777089c2608fa799a1509f22b34fed6801
Ren Usuda <rururu.developer@gmail.com>
2025-11-03 23:20:53 +0900

    test: Use platformDispatcher.localesTestValue in localization tests

--- test/screens/settings_screen_test.dart
+++ test/screens/settings_screen_test.dart
@@ -1,48 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:life_battery/l10n/app_localizations.dart';
 import 'package:life_battery/screens/settings_screen.dart';
+import 'package:life_battery/widgets/common_material_app.dart';
 
 void main() {
   group('Localization tests', () {
     testWidgets('Device locale is English when set to English', (tester) async {
+      tester.platformDispatcher.localesTestValue = [const Locale('en')];
       await tester.pumpWidget(
-        const LocalizedSettingsScreen(locale: Locale('en')),
+        const TestSettingsScreen(),
       );
       expect(find.text('App tutorial'), findsOneWidget);
     });
     testWidgets('Device locale is Japanese when set to Japanese', (
       tester,
     ) async {
+      tester.platformDispatcher.localesTestValue = [const Locale('ja')];
       await tester.pumpWidget(
-        const LocalizedSettingsScreen(locale: Locale('ja')),
+        const TestSettingsScreen(),
       );
       expect(find.text('アプリの使い方'), findsOneWidget);
     });
   });
 }
 
-class LocalizedSettingsScreen extends StatelessWidget {
-  const LocalizedSettingsScreen({required this.locale, super.key});
-
-  final Locale locale;
+class TestSettingsScreen extends StatelessWidget {
+  const TestSettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ProviderScope(
-      child: MaterialApp(
-        home: Localizations(
-          delegates: const [
-            AppLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          locale: locale,
-          child: const SettingsScreen(),
-        ),
+    return const ProviderScope(
+      child: CommonMaterialApp(
+        home: SettingsScreen(),
       ),
     );
   }
